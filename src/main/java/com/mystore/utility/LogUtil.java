e189234076a04178e59c10dfaa7927efeb384e5c
Amit Hens <hens.is.here@gmail.com>
2025-01-29 15:33:47 +0530

    LogUtil small change

--- src/main/java/com/mystore/utility/LogUtil.java
+++ src/main/java/com/mystore/utility/LogUtil.java
@@ -12,12 +12,4 @@ public class LogUtil {
     public static Logger getLogger() {
         return logger;
     }
-
-    public static void info(String message) {
-        logger.info(message);
-    }
-
-    public static void error(String message) {
-        logger.error(message);
-    }
 }
